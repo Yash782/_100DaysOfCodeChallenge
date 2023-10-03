de24a79c718e8f3e6382a740410c3cd6887d46a0
Yash Wadgaonkar <89408284+Yash782@users.noreply.github.com>
2023-10-03 22:12:41 +0530

    Day 2

---
+++ src/Day_2/Factorial.java
@@ -0,0 +1,21 @@
+package Day_2;
+
+import java.util.Scanner;
+// This program returns the factorial of a number
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        System.out.println(fact(n));
+
+    }
+    static int fact(int n){
+        int fac =1;
+        
+        for (int i = 1; i < n; i++) {
+           fac = fac*(i+1);
+        }
+        return fac;
+    }
+}
