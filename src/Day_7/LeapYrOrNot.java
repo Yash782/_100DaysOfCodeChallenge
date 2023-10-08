2954139b4a9c9822a234d2869be7a2af310c6944
Yash Wadgaonkar <89408284+Yash782@users.noreply.github.com>
2023-10-09 00:14:02 +0530

    Day_7

---
+++ src/Day_7/LeapYrOrNot.java
@@ -0,0 +1,19 @@
+package Day_7;
+
+import java.util.Scanner;
+
+public class LeapYrOrNot {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(leap(n)){
+            System.out.println("Leap year");
+        }else {
+            System.out.println("Not a leap year");
+        }
+
+    }
+    static boolean leap(int n){
+        return n % 400 == 0 && n % 4 == 0 && n % 100 != 0;
+    }
+}
