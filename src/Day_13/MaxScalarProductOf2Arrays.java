d6e822833f80ccf77c2ca3e0c2a4d62e695fa3c9
Yash Wadgaonkar <89408284+Yash782@users.noreply.github.com>
2023-10-14 19:40:10 +0530

    Day_13

--- src/Day_13/MaxScalarProductOf2Arrays.java
+++ src/Day_13/MaxScalarProductOf2Arrays.java
@@ -2,27 +2,23 @@ package Day_13;
 
 import java.util.Arrays;
 
-public class MinScalarProductOf2Arrays {
+public class MaxScalarProductOf2Arrays {
     public static void main(String[] args) {
-        // Both arrays must be of same length
         int [] arr1 = {10, 30, 40, 20};
         int [] arr2 = {2, 4, 5, 1};
-        System.out.println(minScalarProduct(arr1, arr2));
+        System.out.println(maxScalarProduct(arr1, arr2));
     }
-    static int minScalarProduct(int [] arr1, int [] arr2){
+    static int maxScalarProduct(int [] arr1, int [] arr2){
         int scalarProduct = 0;
         // Sorting Both arrays so we can easily multiply
         Arrays.sort(arr1);
         Arrays.sort(arr2);
         int j = arr1.length-1;
-        int i = 0;
-        // Now we can iterate one array from start to end and another end to start
+        // Now we can iterate both arrays from end to start and multiply and add them
         while(j >= 0){
-            scalarProduct = arr1[i] * arr2[j] + scalarProduct;
+            scalarProduct = arr1[j] * arr2[j] + scalarProduct;
             j --;
-            i ++;
         }
-
         return scalarProduct;
     }
 }
