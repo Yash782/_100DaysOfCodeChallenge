af9cb5ada0bdb7730f7e43c9ea4f6b9d56d75e3d
Yash Wadgaonkar <89408284+Yash782@users.noreply.github.com>
2023-10-17 23:05:16 +0530

    Day_16

---
+++ src/Day_16/MaxWaterInContainer.java
@@ -0,0 +1,32 @@
+package Day_16;
+//You are given an integer array height of length n. There are n vertical lines drawn such
+// that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+//Find two lines that together with the x-axis form a container, such that the container
+//contains the most water.
+//
+//Return the maximum amount of water a container can store.
+public class MaxWaterInContainer {
+    public static void main(String[] args) {
+        int [] height = {1,1};
+        System.out.println(maxArea(height));
+
+    }
+    static int maxArea(int[] height){
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int currentArea = Math.min(height[left], height[right]) * (right - left);
+            maxArea = Math.max(maxArea, currentArea);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
