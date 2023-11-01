ddb9394eea4e4165220e31c9cec65b603008719e
Yash Wadgaonkar <89408284+Yash782@users.noreply.github.com>
2023-11-02 00:58:09 +0530

    Day 31

--- src/Day_31/SingleNumber.java
+++ src/Day_31/SingleNumber.java
@@ -3,7 +3,7 @@ package Day_31;
 public class SingleNumber {
     public static void main(String[] args) {
         int [] nums = {2,2,1};
-        System.out.println(oneForLoop(nums));
+        System.out.println(singleNumber(nums));
 
     }
     public static int singleNumber(int[] nums) {
@@ -22,18 +22,5 @@ public class SingleNumber {
             }
         }
         return count==1;
-    }
-
-    static int oneForLoop(int [] nums){
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i; j < nums.length; j++) {
-                if(nums[i] != nums[j]){
-                    return nums[i];
-                }
-
-            }
-
-        }
-        return -1;
     }
 }
