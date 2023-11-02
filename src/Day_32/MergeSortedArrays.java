ebb6f5e83ff8ff428176e6e2559eb173f0f08e0f
Yash Wadgaonkar <89408284+Yash782@users.noreply.github.com>
2023-11-03 01:12:38 +0530

    Day 32

---
+++ src/Day_32/MergeSortedArrays.java
@@ -0,0 +1,21 @@
+package Day_32;
+
+
+import java.util.Arrays;
+
+public class MergeSortedArrays {
+    public static void main(String[] args) {
+        int [] arr1 = {1,2,3,0,0,0};
+        int [] arr2 = {2,5,6};
+        merge(arr1, 3, arr2, 3);
+    }
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        for (int i = 0; i <n ; i++) {
+            nums1[m+i] = nums2[i];
+        }
+        Arrays.sort(nums1);
+        System.out.println(Arrays.toString(nums1));
+    }
+
+}
