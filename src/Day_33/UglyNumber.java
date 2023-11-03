38a121506d5295766bc3519a3939b31b8083d8b5
Yash Wadgaonkar <89408284+Yash782@users.noreply.github.com>
2023-11-04 00:54:51 +0530

    Day 33

---
+++ src/Day_33/UglyNumber.java
@@ -0,0 +1,26 @@
+package Day_33;
+
+public class UglyNumber {
+    public static void main(String[] args) {
+        int n = 14;
+        System.out.println(isUgly(n));
+
+    }
+    public static boolean isUgly(int n) {
+
+        if(n<1)
+            return false;
+        while(n%2==0){
+            n=n/2;
+        }
+        while(n%3==0){
+            n=n/3;
+        }
+        while(n%5 ==0){
+            n=n/5;
+        }
+
+        return n==1;
+
+    }
+}
