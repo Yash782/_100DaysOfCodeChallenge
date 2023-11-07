6af1be805998c5847293ec4aad5274ec2a23d75a
Yash Wadgaonkar <89408284+Yash782@users.noreply.github.com>
2023-11-08 00:40:51 +0530

    Day 37

---
+++ src/Day_37/CountingSort1.java
@@ -0,0 +1,28 @@
+package Day_37;
+//https://www.hackerrank.com/challenges/one-month-preparation-kit-countingsort1/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-one
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CountingSort1 {
+    public static void main(String[] args) {
+        int n = 100;
+        List<Integer> arr = Arrays.asList(1,2,3,4,5,6,7,7,7,8,9,10,11,12);
+        System.out.println(countingSort(arr));
+
+    }
+    public static List<Integer> countingSort(List<Integer> arr) {
+        // Write your code here
+        ArrayList<Integer> ansList = new ArrayList<>();
+        int j = 0;
+        int [] indexArr = new int[100];
+        for (int i : arr) {
+            j = i;
+            indexArr[j] += 1;
+        }
+        for(int i : indexArr){
+            ansList.add(i);
+        }
+        return ansList;
+    }
+}
