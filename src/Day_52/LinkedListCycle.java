ba7c71b62b9b67dc816b7678d1586711b57117a2
Yash Wadgaonkar <89408284+Yash782@users.noreply.github.com>
2023-11-26 23:40:56 +0530

    Day 52

---
+++ src/Day_52/LinkedListCycle.java
@@ -0,0 +1,21 @@
+package Day_52;
+//https://leetcode.com/problems/linked-list-cycle/
+
+//This code dosen't work here it is just what I did on leetcode
+public class LinkedListCycle {
+    /*public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast != null && fast.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if(fast == slow){
+                return true;
+            }
+
+        }
+        return false;
+    }*/
+}
