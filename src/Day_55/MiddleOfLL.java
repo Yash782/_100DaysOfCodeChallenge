1cbcbb7d12cc3fe7a25e63619f61cebeb80c969b
Yash Wadgaonkar <89408284+Yash782@users.noreply.github.com>
2023-11-29 20:20:46 +0530

    Day 55

---
+++ src/Day_55/MiddleOfLL.java
@@ -0,0 +1,17 @@
+package Day_55;
+//https://leetcode.com/problems/middle-of-the-linked-list/description/
+
+//Solved on leetcode;
+public class MiddleOfLL {
+    /*public ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+
+    }*/
+}
