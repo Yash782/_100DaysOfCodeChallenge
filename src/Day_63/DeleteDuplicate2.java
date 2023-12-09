f7c1ee63352c3a1a7f5711107cfac348361cf462
Yash Wadgaonkar <89408284+Yash782@users.noreply.github.com>
2023-12-10 00:04:09 +0530

    Day 63

---
+++ src/Day_63/DeleteDuplicate2.java
@@ -0,0 +1,21 @@
+package Day_63;
+
+public class DeleteDuplicate2 {
+    /*public ListNode deleteDuplicates(ListNode head) {
+        ListNode tempHead = new ListNode(0, head);
+        ListNode prev = tempHead;
+
+        while(head != null){
+            if(head.next != null && head.val == head.next.val){
+                while(head.next != null && head.val == head.next.val){
+                    head = head.next;
+                }
+                prev.next = head.next;
+            }else{
+                prev = prev.next;
+            }
+            head = head.next;
+        }
+        return tempHead.next;
+    }*/
+}
