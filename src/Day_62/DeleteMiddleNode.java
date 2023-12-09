9d6ed9e9824a0694bc14aa0ae8683391e82b8082
Yash Wadgaonkar <89408284+Yash782@users.noreply.github.com>
2023-12-09 22:10:24 +0530

    Day 62

---
+++ src/Day_62/DeleteMiddleNode.java
@@ -0,0 +1,24 @@
+package Day_62;
+//https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/submissions/1114587100/
+//Solved on leetcode
+public class DeleteMiddleNode {
+    /*public ListNode deleteMiddle(ListNode head) {
+        if(head == null){
+            return head;
+        }
+        if(head.next == null){
+            return null;
+        }
+        ListNode prev = null;
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast != null && fast.next != null){
+            prev = slow;
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        prev.next = slow.next;
+        return head;
+    }*/
+}
